c1c8f23dacfda8f478eaf0ffd26d4c8a9c935ede
Mateusz <matsoj01@gmail.com>
2023-02-10 15:17:23 +0000

    footer for mobile done

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,50 @@
 const burgerBtn = document.querySelector(".nav__burgerBtn");
 const mobileNav = document.querySelector(".nav__list");
+const dropdowns = document.querySelectorAll('.footer__dropdown')
 
 function showNav() {
 	mobileNav.classList.toggle("showNav");
 }
 
 burgerBtn.addEventListener("click", showNav);
+
+dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector('.footer__select')    
+  const caret = dropdown.querySelector('.footer__caret')    
+  const menu = dropdown.querySelector('.footer__list')    
+  const options = dropdown.querySelectorAll('.footer__link')    
+  const selected = dropdown.querySelectorAll('.footer__selected')    
+
+  select.addEventListener('click', () => {
+    select.classList.toggle('select-clicked')
+    caret.classList.toggle('caret-rotate')
+    menu.classList.toggle('menu-open')
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      selected.innerText = option.innerText;
+      select.classList.remove('selected-clicked');
+      caret.classList.remove('caret-rotate')
+      menu.classList.remove('menu-open')
+
+      options.forEach(option => {
+        option.classList.remove('active')
+      })
+      option.classList.add('active')
+    })
+  })
+})
+
+
+
+
+
+
+
+
+
+// slider
 
 const swiper = new Swiper(".mySwiper", {
 	// Default parameters
